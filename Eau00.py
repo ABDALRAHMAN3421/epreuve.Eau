c939006db4baee06544215c4a6450e0a6954f437
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-10-14 10:30:55 +0400

    Update Eau00.py

--- Eau00.py
+++ Eau00.py
@@ -2,17 +2,17 @@ def three_digit_combination():
     result = ''
 
     for number in range(48, 58): 
-        for number2 in range(number + 1, 58):
-            for number3 in range(number2 + 1, 58):
-                result += chr(number) + chr(number2) + chr(number3) + ','
+        for number2 in range(number + 1, 58):  
+            for number3 in range(number2 + 1, 58):  
+                result += str(number - 48) + str(number2 - 48) + str(number3 - 48) + ','
 
     return result[:-1]  
 
-def error_without_argument(argument=None):
-    if argument:
-        print("Error: The argument is not necessary for this script.")
-    else:
-        print(three_digit_combination())
-
-error_without_argument()
-
+def main():
+    user_input = input("Please enter some input: ")
+    if not user_input():
+        print("Error: No arguments provided. Please provide input.")
+        return  
+    
+    result = three_digit_combination()
+    print(result)

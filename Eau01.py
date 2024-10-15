28b1e25115126edf028412ec0f2e24e03ba7557f
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-10-15 11:46:16 +0400

    Update Eau01.py

--- Eau01.py
+++ Eau01.py
@@ -1,61 +1,28 @@
-import sys
+def display_combinations():
+    combinations = [] 
+    for i in range(100):  
+        for j in range(i + 1, 100):  
+            
+            if i < 10:
+                first_num = "0" + str(i)  
+            else:
+                first_num = str(i)
+                
+            if j < 10:
+                second_num = "0" + str(j)  
+            else:
+                second_num = str(j)
+                
+            combination = first_num + " " + second_num
+            combinations.append(combination) 
+                                                                                                                                    
+    result = ""
+    for index in range(len(combinations)):
+        result += combinations[index]
+        if index < len(combinations) - 1:  
+            result += ", "
+    
+    print(result)
 
-def reverse_phrase(words):
-    return ' '.join(words[::-1])
-
-def get_input():
-    if len(sys.argv) > 2:
-        return ' '.join(sys.argv[2:])
-    else:
-        return input("Enter a phrase please: ")
-
-def display_result(phrase):
-    print("Reversed Phrase:")
-    for word in phrase.split():
-        print(word)
-
-def handle_error(message):
-    print(f"Error: {message}")
-
-def command_line_argument():
-    if len(sys.argv) > 1:
-        arg = sys.argv[1].lower()
-        if arg in ['true', '1', 'yes', 'y']:
-            return True
-        elif arg in ['false', '0', 'no', 'n']:
-            return False
-        else:
-            return None
-    else:
-        return True
-
-def display_combinations(print_combinations):
-    if print_combinations:
-        print("Displaying combinations...")
-    else:
-        print("Not displaying combinations...")
-
-def main():
-    print_combinations = command_line_argument()
-
-    if print_combinations is None:
-        handle_error(f"Invalid argument: {sys.argv[1]}")
-        print("Usage: python script_name.py [true or false]")
-        return
-
-    display_combinations(print_combinations)
-
-    if print_combinations:
-        frase = get_input()
-        if not frase:
-            handle_error("No input provided")
-            return
-
-        words = frase.split()
-        if not words:
-            handle_error("No words to reverse")
-            return
-
-        reversed_phrase = reverse_phrase(words)
-        display_result(reversed_phrase)
+display_combinations()
 

4f061c8883e545f4417c9f58502dab102c178dec
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-11-19 12:09:53 +0400

    Update Eau03.py

--- Eau03.py
+++ Eau03.py
@@ -1,41 +1,30 @@
 import sys
 
-def fibonacci_nth_element(n):
+def fibonacci_with_elemnt(n):
     if n == 0:
         return 0
     if n == 1:
         return 1
+    
+    a = 0
+    b = 1
 
-    a, b = 0, 1
 
-    for _ in range(2, n + 1):
-        a, b = b, a + b
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
 
     return b
 
-def is_number(argument):
-    if not isinstance(argument, int) or argument < 0:
+def get_argument():
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) < 0:
         print("Erreur : l'argument doit être un nombre entier positif")
         sys.exit(1)
-
-
-
-def get_argument():
-    if len(sys.argv) < 2:
-        print("Erreur : veuillez fournir un argument")
-        sys.exit(1)
-    try:
-        argument = int(sys.argv[1])
-    except ValueError:
-        print("Erreur : l'argument doit être un nombre entier")
-        sys.exit(1)
-    return argument
+    return int(sys.argv[1])
 
 
 argument = get_argument()
-is_number(argument)
-
-result = fibonacci_nth_element(argument)
-
+result = fibonacci_with_elemnt(argument)
 
 print(result)

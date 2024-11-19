1654ae3f524ecbc4879b220255ba2c8a27dbd58f
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-11-19 12:05:25 +0400

    Update Eau01.py

--- Eau01.py
+++ Eau01.py
@@ -1,30 +1,12 @@
-def afficher_combination():
-    combinations = [] 
-    for i in range(100):  
-        for j in range(i + 1, 100):  
-            
-            
-            if i < 10:
-                first_num = "0" + str(i)  
-            else:
-                first_num = str(i)
-                
-            if j < 10:
-                second_num = "0" + str(j)  
-            else:
-                second_num = str(j)
-                
-            
-            combination = first_num + " " + second_num
-            combinations.append(combination) 
-                                                                                                                                    
-    result = ""
-    for index in range(len(combinations)):
-        result += combinations[index]
-        if index < len(combinations) - 1:  
-            result += ", "
-    
-    
-    print(result)
+def two_nembers():
+    for a in range (100):
+        for b in range(a+ 1, 100):
+            print(f"{format_number(a)} {format_number(b)},",end=" ")
 
-afficher_combination()
+
+def format_number(number):
+    return f"{number:02}"
+
+
+two_nembers()
+

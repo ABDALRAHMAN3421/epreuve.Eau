5513b719d0146ca5df200c34faa71554abba4660
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-12-01 18:14:22 +0400

    Update Eau14.py

--- Eau14.py
+++ Eau14.py
@@ -1,31 +1,26 @@
 import sys
 
-def sort_elements(elements):
-    if not elements:
-        print("Error: No elements to sort.")
-        return None
-    new_elements = elements.split()
-    new_elements = sorted(new_elements)
-    return new_elements
 
-def main():
-    if len(sys.argv) > 1:
-        elements = ' '.join(sys.argv[1:])
-        sorted_elements = sort_elements(elements)
-        if sorted_elements is not None:
-            print("Elements sorted by ASCII order: ")
-            print(sorted_elements)
-    else:
-        while True:
-            elements = input("Enter a phrase or a sequence of characters (or type 'exit' to quit): ")
-            
-            if elements.lower() == 'exit':
-                print("Exiting program.")
-                break
-            
-            sorted_elements = sort_elements(elements)
-            if sorted_elements is not None:
-                print("Elements sorted by ASCII order: ")
-                print(sorted_elements)
+def get_arguments():
+    arguments = sys.argv[1:]  
+    if not arguments:
+        print("Error: No arguments provided.")
+        sys.exit(1)  
+    return arguments
 
-    main()
+
+def sort_ascii(arguments):
+    for i in range(len(arguments) - 1):
+        for j in range(len(arguments) - i - 1):
+            if list(map(ord, arguments[j])) > list(map(ord, arguments[j + 1])):
+                arguments[j], arguments[j + 1] = arguments[j + 1], arguments[j]
+    return arguments
+
+
+arguments = get_arguments()
+sorted_arguments = sort_ascii(arguments)
+result = " ".join(sorted_arguments)
+
+
+print(result)
+

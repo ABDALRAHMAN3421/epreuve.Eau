ec6c9bdc0514d5db084e41e69dfc1a58f2ddc1b2
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-12-02 11:42:07 +0400

    Update Eau10.py

--- Eau10.py
+++ Eau10.py
@@ -1,23 +1,26 @@
 import sys
 
-def premier_index(tableau, element_recherche):   
-    for i in range(len(tableau)):               
-        if tableau[i] == element_recherche:      
-            return i, True                             
-    return -1, False                              
+def find_index(arg):
+    result = -1
+    last_word = arg[-1]
+    for i in range(len(arg) - 1):
+        if arg[i] == last_word:
+            result = i
+            break
+    return result
 
-def main_logic():
-    tableau_input = input("Entrez les éléments séparés par des espaces: ")
-    element_recherche = input("Entrez l'élément recherché: ")
-
-    if tableau_input and element_recherche:
-        tableau = tableau_input.split()
-        index, found = premier_index(tableau, element_recherche)
-        if found:
-            print("Premier index de l'élément recherché:", index)
-        else:
-            print("L'élément recherché n'a pas été trouvé.")
+def get_argument():
+    if len(sys.argv) > 1:
+        argument = sys.argv[1:]
+        return argument
     else:
-        print("Une erreur s'est produite: les entrées ne doivent pas être vides")
+        print("Error: No arguments provided.")
+        return []
+        
+my_argument = get_argument()
 
-main_logic()
+if my_argument:
+    result = find_index(my_argument)
+    print(result)
+else:
+    print("Error: No valid argument found.")

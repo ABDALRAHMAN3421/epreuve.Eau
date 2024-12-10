af6c18f01c783c0a0708115fb8e206b98494f44d
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-12-10 10:26:12 +0400

    Update Eau08.py

--- Eau08.py
+++ Eau08.py
@@ -1,17 +1,24 @@
-def get_age():
-    while True:
-        age, success = try_get_age()
-        if success:
-            print("True")
-            break
-        else:
-            print("False")
+import sys
 
-def try_get_age():
-    user_input = input("Enter a number: ")
-    if user_input.isdigit():
-        return int(user_input), True
-    else:
-        return None, False
+def is_string_of_number(arg):
+    for char in arg:
+        if not char.isdigit():
+            return False
+    return True
 
-get_age()
+def number_of_argument_and_validity():
+    if len(sys.argv) != 2:
+        print("Erreur : Le programme prend un seul argument.")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("Erreur : L'argument ne doit contenir que des chiffres.")
+        sys.exit(1)
+
+
+def get_argument():
+    return sys.argv[1]
+
+
+number_of_argument_and_validity()
+argument = get_argument()
+print(is_string_of_number(argument))
